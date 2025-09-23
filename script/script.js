d8523ac52d4540af7d6524ad4b952548b13e01d5
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-09-23 17:24:08 -0500

    Feat: display arrLibrary

--- script/script.js
+++ script/script.js
@@ -1,30 +1,47 @@
 /* MAIN */
 const arrLibrary = [];
 
+function showBooks(bookList) {
+    const table = document.querySelector('table');
+    const tbody = table.querySelector('tbody');
+
+    bookList.forEach(book => {
+        const tr = document.createElement('tr');
+        
+        for (const item in book) {
+            const td = document.createElement('td');
+            td.textContent = book[item];
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    });
+}
+
+function addBookToLibrary(book) {
+    arrLibrary.push(book);
+}
+
 function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("ERROR! Use the new operator.");
     }
-
+    this.id = crypto.randomUUID(); // Working?
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.id = crypto.randomUUID(); // Working?
-
-    // for testing
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    };
 }
 
-Book.prototype.addBookToLibrary = function () {
-    arrLibrary.push(this);
-}
+
 
 // Manually Added Books
 const book = new Book("The Hobbit", "J.K.K Tolkien", 295, "not read yet");
 const book2 = new Book("The Hobbit2", "J.K.K Tolkien", 295, "not read yet");
 const book3 = new Book("The Hobbit3", "J.K.K Tolkien", 295, "not read yet");
+addBookToLibrary(book);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+showBooks(arrLibrary);
+
 
 /* DISPLAY */
