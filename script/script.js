f706c2c96a632124fc5ba13a99b40705e181f4dc
Sarah Davis <170495238+SarahDavis2@users.noreply.github.com>
2025-09-23 16:25:49 -0500

    Feat: Book constructor

--- script/script.js
+++ script/script.js
@@ -1,14 +1,13 @@
+const arrLibrary = [];
+
 function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("ERROR! Use the new operator.");
     }
+    
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, ${read}`;
-    };
+    this.id = crypto.randomUUID();
 }
-
-const book = new Book("The Hobbit", "J.K.K Tolkien", 295, "not read yet");
